dd6801c35de9ab46b177607e82bc3419900c636b
t100ts <120762080+t100ts@users.noreply.github.com>
2025-07-07 21:11:40 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+| Carpeta         | Archivo              | Contenido inicial |
+| --------------- | -------------------- | ----------------- |
+| /               | `package.json`       | Te lo doy abajo   |
+| /               | `tsconfig.json`      | ✅                 |
+| /               | `vite.config.ts`     | ✅                 |
+| /               | `tailwind.config.js` | ✅                 |
+| public/         | `index.html`         | ✅                 |
+| src/            | `main.tsx`           | como `index.tsx`  |
+| src/            | `App.tsx`            | Te lo doy abajo   |
+| src/styles/     | `index.css`          | ✅                 |
+| src/components/ | `Button.tsx`         | ✅                 |
+| src/components/ | `Input.tsx`          | ✅                 |
+| src/components/ | `Card.tsx`           | ✅                 |
+| src/components/ | `RadioGroup.tsx`     | ✅                 |
+| src/components/ | `Checkbox.tsx`       | ✅                 |
